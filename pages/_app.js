3c98f486c55239500efbb9a0a198db021a99f8b1
Felipe <felipedantas184@gmail.com>
2021-08-09 23:04:40 -0300

    First Commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
+import { ThemeProvider } from 'styled-components'
+import Layout from '../components/Layout'
+import theme from '../styles/theme'
+
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
